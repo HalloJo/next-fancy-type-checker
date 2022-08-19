4d6bf2adeb2ea40ad85028ed8e8df3edc55e75b1
JorikVanRuiswijk <jorik.van.ruiswijk@mediamonks.com>
2022-08-19 15:21:02 +0200

    Add favicon, change color of logo

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,6 +15,10 @@ const Home: NextPage = () => {
         <title>The Next Fancy Type Checker!</title>
         <meta name="description" content="A personal project where you can animate your text to see what is fancy pancy!" />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
 
       <main className={styles.main}>
