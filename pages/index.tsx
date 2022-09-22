05aacbe78ad5cc27a9dd3e937fa03ee8059bfae2
JorikVanRuiswijk <jorik.van.ruiswijk@mediamonks.com>
2022-09-22 10:38:42 +0200

    Add some styling to try out colours

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,10 +25,8 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <div>
-          <Logo />
-          <Output output={output} />
-        </div>
+        <Logo />
+        <Output output={output} />
       </main>
     </div>
   )
